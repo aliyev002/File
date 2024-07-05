a8d225adb06508052cdef6b643238046e376c9fc
aliyev002 <168084470+aliyev002@users.noreply.github.com>
2024-07-05 18:25:34 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,37 +1,47 @@
-﻿using ConsoleApp9.Model;
 
-using System.Text;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Xml;
-void JsonSerializeMethod()
+using ConsoleApp9.Model;
+
+namespace ConsoleApp9
 {
-    new Car { Model = "Priora", Marka = "Lada", Year = 2013 };
-    new Car { Model = "2107", Marka = "Lada", Year = 2011 };
-    new Car { Model = "2106", Marka = "Lada", Year = 1999 };
-
-    List<Car> cars = new List<Car>();
-    foreach (Car car in cars)
+    class Program
     {
-        cars.Add(car);
+        static void Main(string[] args)
+        {
+            JsonSerializeMethod();
+        }
+
+        static void JsonSerializeMethod()
+        {
+            
+            List<Car> cars = new List<Car>
+            {
+                new Car { Model = "Priora", Marka = "Lada", Year = 2013 },
+                new Car { Model = "2107", Marka = "Lada", Year = 2011 },
+                new Car { Model = "2106", Marka = "Lada", Year = 1999 }
+            };
+
+            
+            CarGalery galery = new CarGalery("Avtos", cars);
+
+            
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                WriteIndented = true 
+            };
+
+            
+            string jsonString = JsonSerializer.Serialize(galery, options);
+
+            
+            Console.WriteLine(jsonString);
+
+            
+            File.WriteAllText("cars.json", jsonString);
+        }
     }
-    CarGalery galery = new CarGalery("Avtos", cars);
-
-
-
-
-    
-    JsonSerializerOptions options = new JsonSerializerOptions
-    {
-        WriteIndented = true 
-    };
-
-    
-    string jsonString = JsonSerializer.Serialize(galery, options);
-
-    
-    Console.WriteLine(jsonString);
-
-    
-    File.WriteAllText("cars.json", jsonString);
 }
